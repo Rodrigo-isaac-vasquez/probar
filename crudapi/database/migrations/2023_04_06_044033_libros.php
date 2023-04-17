b302826afcf5bca5d2b1804fd042e67887d91732
Rodrigo <rodryv2004@gmail.com>
2023-04-17 16:18:22 -0600

    V0.1

---
+++ crudapi/database/migrations/2023_04_06_044033_libros.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',100);
+            $table->string('apellidos',100);
+            $table->integer('edad');
+            $table->decimal('salario',6,2);
+            $table->string('imagen', 2500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('libros');
+    }
+};
