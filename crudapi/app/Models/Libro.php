a89e79df2497518658136dbb359f25e55e7f980c
Rodrigo <rodryv2004@gmail.com>
2023-04-17 16:23:41 -0600

    Cambios1

--- crudapi/app/Models/Libro.php
+++ crudapi/app/Models/Libro.php
@@ -4,7 +4,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Libro extends Model{
-    protected $table = "libros";
+
+   protected $table = "libros";
 
     // protected $fillable = [];
 
